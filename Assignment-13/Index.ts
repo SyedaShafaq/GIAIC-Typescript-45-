f730a4e2a9ca65d02edd91249e249acd63ea0250
SyedaShafaq <shafaqfatima161@gmail.com>
2024-03-22 21:14:18 +0500

    45-Questions

--- Assignment-13/Index.ts
+++ Assignment-13/Index.ts
@@ -1,12 +1,21 @@
 //Question-15 (Changing guest list)
 //You just heard that one of your guests can’t make the dinner
-
-let guestList:string[] = [`Zainab`,`Zara`,`Anaya`,`Shifa`];
+// Start with your program from Exercise 14
+let guestList: string[] = [`Zainab`, `Zara`, `Anaya`, `Shifa`];
 console.log(guestList);
 
 // need to send out a new set of invitations
 //With the help of splice
-let newGuestList:string[] = guestList.splice(1 , 1 , `Aiman`);
-console.log(newGuestList);
+let newGuestList: string[] = guestList.splice(1, 1, `Aiman`);
 console.log(guestList);
 
+//Print a second set of invitation messages, one for each person who is still in your list
+let message: string = `Come join us for a night of laughter, great conversations, and mouth-watering dishes`;
+
+console.log(`${guestList[0]}! ${message}`);
+console.log(`${guestList[1]}! ${message}`);
+console.log(`${guestList[2]}! ${message}`);
+console.log(`${guestList[3]}! ${message}`);
+
+//Can't make the dinner
+console.log(newGuestList);
