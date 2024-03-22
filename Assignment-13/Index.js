32224604bdee26d3a63807e0daf52f14fc30fcaa
SyedaShafaq <shafaqfatima161@gmail.com>
2024-03-22 21:16:58 +0500

    45-Questions

--- Assignment-13/Index.js
+++ Assignment-13/Index.js
@@ -6,7 +6,6 @@ console.log(guestList);
 // need to send out a new set of invitations
 //With the help of splice
 let newGuestList = guestList.splice(1, 1, `Aiman`);
-console.log(newGuestList);
 console.log(guestList);
 //Print a second set of invitation messages, one for each person who is still in your list
 let message = `Come join us for a night of laughter, great conversations, and mouth-watering dishes`;
@@ -14,4 +13,6 @@ console.log(`${guestList[0]}! ${message}`);
 console.log(`${guestList[1]}! ${message}`);
 console.log(`${guestList[2]}! ${message}`);
 console.log(`${guestList[3]}! ${message}`);
+//Can't make the dinner
+console.log(newGuestList);
 export {};
